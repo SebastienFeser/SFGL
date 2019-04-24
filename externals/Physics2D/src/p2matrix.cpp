24a685f3055582dba205d9045ceb0154a021083b
Cédric Chambaz <cdricchambaz@yahoo.fr>
2019-04-24 12:27:55 +0200

    Implemented matrix operators

--- externals/Physics2D/src/p2matrix.cpp
+++ externals/Physics2D/src/p2matrix.cpp
@@ -30,36 +30,57 @@ p2Mat22::p2Mat22()
 
 p2Mat22::p2Mat22(p2Vec2 r1, p2Vec2 r2)
 {
+	rows[0] = r1;
+	rows[1] = r2;
 }
 
 p2Mat22 p2Mat22::operator+(p2Mat22 m1)
 {
-	return p2Mat22();
+	p2Vec2 r1 = p2Vec2(rows[0].x + m1.rows[0].x, rows[0].y + m1.rows[0].y);
+	p2Vec2 r2 = p2Vec2(rows[1].x + m1.rows[1].x, rows[1].y + m1.rows[1].y);
+	return p2Mat22(r1, r2);
 }
 
 p2Mat22 p2Mat22::operator-(p2Mat22 m1)
 {
-	return p2Mat22();
+	p2Vec2 r1 = p2Vec2(rows[0].x - m1.rows[0].x, rows[0].y - m1.rows[0].y);
+	p2Vec2 r2 = p2Vec2(rows[1].x - m1.rows[1].x, rows[1].y - m1.rows[1].y);
+	return p2Mat22(r1, r2);
 }
 
 p2Mat22 p2Mat22::operator*(p2Mat22 m1)
 {
-	return p2Mat22();
+	p2Vec2 r1 = p2Vec2(rows[0].x, rows[0].y);
+	p2Vec2 r2 = p2Vec2(rows[1].x, rows[1].y);
+
+	p2Vec2 c1 = p2Vec2(m1.rows[0].x, m1.rows[0].y);
+	p2Vec2 c2 = p2Vec2(m1.rows[1].x, m1.rows[1].y);
+
+	p2Vec2 result1 = p2Vec2(p2Vec2().Dot(r1, c1), p2Vec2().Dot(r1, c2));
+	p2Vec2 result2 = p2Vec2(p2Vec2().Dot(r2, c1), p2Vec2().Dot(r2, c2));
+
+	return p2Mat22(result1, result2);
 }
 
 p2Vec2 p2Mat22::operator*(p2Vec2 v)
 {
-	return p2Vec2();
+	float x = (rows[0].x * v.x) + (rows[0].y * v.y);
+	float y = (rows[1].x * v.x) + (rows[1].y * v.y);
+	return p2Vec2(x, y);
 }
 
 p2Mat22 p2Mat22::operator*(float f)
 {
-	return p2Mat22();
+	p2Vec2 r1 = p2Vec2(rows[0].x * f, rows[0].y * f);
+	p2Vec2 r2 = p2Vec2(rows[1].x * f, rows[1].y * f);
+	return p2Mat22(r1, r2);
 }
 
 p2Mat22 p2Mat22::operator/(float f)
 {
-	return p2Mat22();
+	p2Vec2 r1 = p2Vec2(rows[0].x / f, rows[0].y / f);
+	p2Vec2 r2 = p2Vec2(rows[1].x / f, rows[1].y / f);
+	return p2Mat22(r1, r2);
 }
 
 p2Mat22 p2Mat22::Invert()
@@ -78,36 +99,66 @@ p2Mat33::p2Mat33()
 
 p2Mat33::p2Mat33(p2Vec3 r1, p2Vec3 r2, p2Vec3 r3)
 {
+	rows[0] = r1;
+	rows[1] = r2;
+	rows[2] = r3;
 }
 
 p2Mat33 p2Mat33::operator+(p2Mat33 m1)
 {
-	return p2Mat33();
+	p2Vec3 r1 = p2Vec3(rows[0].x + m1.rows[0].x, rows[0].y + m1.rows[0].y, rows[0].z + m1.rows[0].z);
+	p2Vec3 r2 = p2Vec3(rows[1].x + m1.rows[1].x, rows[1].y + m1.rows[1].y, rows[1].z + m1.rows[1].z);
+	p2Vec3 r3 = p2Vec3(rows[2].x + m1.rows[2].x, rows[2].y + m1.rows[2].y, rows[2].z + m1.rows[2].z);
+	return p2Mat33(r1, r2, r3);
 }
 
 p2Mat33 p2Mat33::operator-(p2Mat33 m1)
 {
-	return p2Mat33();
+	p2Vec3 r1 = p2Vec3(rows[0].x - m1.rows[0].x, rows[0].y - m1.rows[0].y, rows[0].z - m1.rows[0].z);
+	p2Vec3 r2 = p2Vec3(rows[1].x - m1.rows[1].x, rows[1].y - m1.rows[1].y, rows[1].z - m1.rows[1].z);
+	p2Vec3 r3 = p2Vec3(rows[2].x - m1.rows[2].x, rows[2].y - m1.rows[2].y, rows[2].z - m1.rows[2].z);
+	return p2Mat33(r1, r2, r3);
 }
 
 p2Mat33 p2Mat33::operator*(p2Mat33 m1)
 {
-	return p2Mat33();
+	p2Vec3 r1 = p2Vec3(rows[0].x, rows[0].y, rows[0].z);
+	p2Vec3 r2 = p2Vec3(rows[1].x, rows[1].y, rows[1].z);
+	p2Vec3 r3 = p2Vec3(rows[2].x, rows[2].y, rows[2].z);
+
+	p2Vec3 c1 = p2Vec3(m1.rows[0].x, m1.rows[0].y, m1.rows[0].z);
+	p2Vec3 c2 = p2Vec3(m1.rows[1].x, m1.rows[1].y, m1.rows[1].z);
+	p2Vec3 c3 = p2Vec3(m1.rows[2].x, m1.rows[2].y, m1.rows[2].z);
+
+	p2Vec3 result1 = p2Vec3(p2Vec3().Dot(r1, c1), p2Vec3().Dot(r1, c2), p2Vec3().Dot(r1, c3));
+	p2Vec3 result2 = p2Vec3(p2Vec3().Dot(r2, c1), p2Vec3().Dot(r2, c2), p2Vec3().Dot(r2, c3));
+	p2Vec3 result3 = p2Vec3(p2Vec3().Dot(r3, c1), p2Vec3().Dot(r3, c2), p2Vec3().Dot(r3, c3));
+
+	return p2Mat33(result1, result2, result3);
 }
 
-p2Vec3 p2Mat33::operator*(p2Vec3)
+p2Vec3 p2Mat33::operator*(p2Vec3 v)
 {
-	return p2Vec3();
+	float x = (rows[0].x * v.x) + (rows[0].y * v.y) + (rows[0].z * v.z);
+	float y = (rows[1].x * v.x) + (rows[1].y * v.y) + (rows[1].z * v.z);
+	float z = (rows[2].x * v.x) + (rows[2].y * v.y) + (rows[2].z * v.z);
+	return p2Vec3(x, y, z);
 }
 
 p2Mat33 p2Mat33::operator*(float f)
 {
-	return p2Mat33();
+	p2Vec3 r1 = p2Vec3(rows[0].x * f, rows[0].y * f, rows[0].z * f);
+	p2Vec3 r2 = p2Vec3(rows[1].x * f, rows[1].y * f, rows[1].z * f);
+	p2Vec3 r3 = p2Vec3(rows[2].x * f, rows[2].y * f, rows[2].z * f);
+	return p2Mat33(r1, r2, r3);
 }
 
 p2Mat33 p2Mat33::operator/(float f)
 {
-	return p2Mat33();
+	p2Vec3 r1 = p2Vec3(rows[0].x / f, rows[0].y / f, rows[0].z / f);
+	p2Vec3 r2 = p2Vec3(rows[1].x / f, rows[1].y / f, rows[1].z / f);
+	p2Vec3 r3 = p2Vec3(rows[2].x / f, rows[2].y / f, rows[2].z / f);
+	return p2Mat33(r1, r2, r3);
 }
 
 p2Mat33 p2Mat33::Invert()
